a1e90d1d96bcbeb20b5db174a64051f867810c26
Fablab-Quenn <antalinguerefab@gmail.com>
2025-02-23 10:55:17 +0000

    Update Mon_Appli.py

--- Mon_Appli.py
+++ Mon_Appli.py
@@ -191,10 +191,7 @@ if menu_option == "Voir les datasets existants":
 # Section "À propos de moi"
 if menu_option == "À propos de moi":
     st.write("## 👋 À propos de moi")
-    
-    # Image
-    #st.image(r"C:\Users\bmd tech\Desktop\AT\at2222.jpg", width=800
-    st.image("images/at2222.jpg", width=800)
+
     
     # Description personnelle
     st.write("""
